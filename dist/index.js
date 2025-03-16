4548129f3de8e95278f8635b1a33b3dc6ca0b08e
GabrielCatanzaro <gabrielgomescatanzaro@gmail.com>
2025-03-16 16:24:11 -0300

    Exercicio 3 - Classe e Instância

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,12 @@
-function mensagemPersonalizada(nome) {
-    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+class Carro {
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    detalhes() {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
 }
-console.log(mensagemPersonalizada("Ana"));
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-function mensagemPersonalizada(nome: string): string {
-    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number) {}
+    detalhes(): string {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
 }
-console.log(mensagemPersonalizada("Ana"));
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
